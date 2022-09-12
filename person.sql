5ec1705ce3fc04c9fc7abef2ffb024a3a26a9e5c
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-12 15:09:40 -0600

    second

---
+++ person.sql
@@ -0,0 +1,50 @@
+-- -- create table person(
+-- -- id SERIAL primary key,
+-- -- name varchar(60)NOT NULL,
+-- -- age integer,
+-- -- height integer,
+-- -- city varchar(40),
+-- -- favortie_color varchar(40)
+-- -- );
+
+-- insert into person(name, age, height, city, favortie_color)
+-- values ('Jack', 32, 60, 'Albuquerque', 'blue'), ('David', 80, 65, 'Seattle', 'yellow'), ('Samantha', 29, 87, 'Las Vegas', 'green'), ('Mars', 33, 67, 'Seattle', 'blue');
+
+-- select * from person 
+-- order by height desc;
+
+-- select * from person 
+-- order by height ;.
+
+-- select * from person
+-- order by age desc;
+
+-- select * from person
+-- where age > 20;
+
+-- select * from person 
+-- where age = 18;
+
+-- select * from person
+-- where age < 20 and age > 30;
+
+-- select * from person
+-- where age != 27
+
+-- select * from person
+-- where favortie_color != 'red'
+
+-- select * from person 
+-- where favortie_color != 'red' and favortie_color !='blue'
+
+-- select * from person
+-- where favortie_color = 'orange' or favortie_color = 'green'
+
+-- select * from person
+-- where favortie_color in ('orange', 'green', 'blue')
+
+-- select * from person
+-- where favortie_color in ('yellow', 'purple')
+
+
+

a0d57d24a2b27855e3272153083310fa197b513e
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-12 16:12:04 -0600

    fourth

---
+++ invoice.sql
@@ -0,0 +1,24 @@
+
+-- select max(total) from invoice;
+
+-- select min(total) from invoice;
+
+-- select * from invoice
+-- where total > 5;
+
+
+-- select count(*) from invoice
+-- where total < 5;
+
+-- select count(*) from invoice 
+-- where billing_state in('CA', 'TX', 'AZ')
+
+-- select avg(total) from invoice;
+
+-- select sum(total) from invoice;
+
+--  update invoice
+--  set total = 24
+--  where invoice_id=5;
+
+-- select * from invoice
